59e9c4f335c026a7cf8786a3f176486d981c3074
Christopher Williams <itschriswilliams@gmail.com>
2013-02-02 22:46:35 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Billboardv1::Application.config.secret_token = '12be10a541f437a3009d0b01465457f7263e597bc6ef7530f40ef94181001c59d9177ec809d83a037270a7f48cbe2ecb7f9fb2aa93753b912c4bd318de42a884'
